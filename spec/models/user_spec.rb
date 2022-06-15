57dde51644466644d86ec38c53e7a33b4e8cc967
hukai <huzhengen@163.com>
2022-06-15 14:22:01 +0800

    请求验证码频繁的测试代码从 api 文档的测试里移出去

--- spec/models/user_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  it 'has a email' do
-    user = User.new(email: '1@qq.com')
-    expect(user.email).to eq('1@qq.com')
-  end
-end
--- spec/requests/validation_codes_spec.rb
+++ spec/requests/validation_codes_spec.rb
@@ -2,9 +2,15 @@ require "rails_helper"
 
 RSpec.describe "ValidationCodes", type: :request do
   describe "验证码" do
-    it "发送验证码" do
+    it "发送验证码成功" do
       post api_v1_validation_codes_path params: { email: "1@qq.com" }
       expect(response).to have_http_status(200)
+    end
+    it "发送验证码太频繁，返回 429" do
+      post api_v1_validation_codes_path params: { email: "1@qq.com" }
+      expect(response).to have_http_status(200)
+      post api_v1_validation_codes_path params: { email: "1@qq.com" }
+      expect(response).to have_http_status(429)
     end
   end
 end
