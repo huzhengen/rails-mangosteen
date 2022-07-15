a48672eeb915660ca6b0360dc2a222faa27ca485
huzhengen <huzhengen@163.com>
2022-07-15 11:04:01 +0800

    API 文档的 jwt

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -3,32 +3,26 @@ require "rspec_api_documentation/dsl"
 
 resource "账目" do
   get "/api/v1/items" do
-    parameter :page, '分页参数', required: false
-    parameter :created_after, '创建时间起始', required: false
-    parameter :created_before, '创建时间结束', required: false
+    authentication :basic, :auth
+    parameter :page, "页码"
+    parameter :created_after, "创建时间起始"
+    parameter :created_before, "创建时间结束"
     with_options :scope => :resource do
-      response_field :id, 'ID'
-      response_field :amount, '金额(分)'
+      response_field :id, "ID"
+      response_field :amount, "金额(分)"
     end
 
     let(:created_after) { "2010-01-01" }
     let(:created_before) { "2011-01-01" }
+    let(:current_user) { User.create! email: "1@qq.com"}
+    let(:auth) { "Bearer #{current_user.generate_jwt}" }
 
     example "获取账目" do
-      user1 = User.create! email: '1@qq.com'
-      11.times {Item.create amount: 100, created_at: '2010-01-09', user_id: user1.id}
-
-      jwt = ''
-      no_doc do
-        client.post 'api/v1/session', email: user1.email, code: '123456'
-        json = JSON.parse response_body
-        jwt = json['jwt']
-      end
-      header 'Authorization', "Bearer #{jwt}"
+      11.times { Item.create amount: 100, created_at: "2010-01-09", user_id: current_user.id }
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
-      expect(json['resources'].size).to eq 10
+      expect(json["resources"].size).to eq 10
     end
   end
 end
